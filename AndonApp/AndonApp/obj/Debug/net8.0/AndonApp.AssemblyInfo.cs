39de26eb60230b7bfbffb05ad29ec2dc24c75074
Jon Wright <jonnywwright@googlemail.com>
2026-05-01 09:35:46 +0100

    Added review screen and resized cards

--- AndonApp/AndonApp/obj/Debug/net8.0/AndonApp.AssemblyInfo.cs
+++ AndonApp/AndonApp/obj/Debug/net8.0/AndonApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AndonApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74384c6a1368ff473857c8d980e53bb82c8ce02d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a5612e9f9a6413b484d5deb6644c76cba51f91d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AndonApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AndonApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
