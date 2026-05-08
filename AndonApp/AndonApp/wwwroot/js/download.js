55874dff0d5e8931c4f677eba8d3a95584bdb24f
Jon Wright <jonnywwright@googlemail.com>
2026-05-08 10:21:24 +0100

    Added History page

---
+++ AndonApp/AndonApp/wwwroot/js/download.js
@@ -0,0 +1,11 @@
+window.downloadFile = function (filename, contentType, bytes) {
+    const blob = new Blob([new Uint8Array(bytes)], { type: contentType });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+};
